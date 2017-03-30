ff13898b56a834488bad126f90e5fc78fe793e85
Radu Alexe <alexeRadu2007@gmail.com>
2017-03-30 15:56:38 +0000

    set stack to be 4KB after the boot sector
    Signed-off-by: Radu Alexe <alexeRadu2007@gmail.com>

--- boot.asm
+++ boot.asm
@@ -6,6 +6,10 @@ start:
 	mov ax, 0x07c0				; Set data segment to be the same as text segment
 	mov ds, ax
 
+	add ax, 0x200				;
+	mov ss, ax				; Set stack segment to start at exactly after boot sector
+	mov sp, 0x1000				; Stack is 4 KB
+
 	mov si, message				; This way ds:si will point to message
 	call print				; Print message to screen
 
