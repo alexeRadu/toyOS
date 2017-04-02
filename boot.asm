f40f6d4846d0bf83b02f7eb79b172dabbf503286
Radu Alexe <alexeRadu2007@gmail.com>
2017-04-02 21:05:37 +0300

    add clear screen function
    Signed-off-by: Radu Alexe <alexeRadu2007@gmail.com>

--- boot.asm
+++ boot.asm
@@ -10,6 +10,8 @@ start:
 	mov ss, ax				; Set stack segment to start at exactly after boot sector
 	mov sp, 0x1000				; Stack is 4 KB
 
+	call cls				; clear the screen
+
 	mov si, message				; This way ds:si will point to message
 	call print				; Print message to screen
 
@@ -34,5 +36,28 @@ print:
 	popa					; Restore contents of registers
 	ret					; Return from print
 
+; clear screen
+cls:
+	pusha
+
+	mov ah, 0x06				; select scroll up page
+	mov al, 0x00				; clear entire window
+	mov bh, 0x0f				; white on black background
+	mov ch, 0x00				;
+	mov cl, 0x00				; start at (0, 0) = top left
+	mov dh, 25				;
+	mov dl, 80				; end at (40, 25) = bottom right
+	int 10h
+
+	mov ah, 0x02				; set cursor position
+	mov dh, 0x00
+	mov dl, 0x00				; at (0, 0)
+	mov bh, 0x00				; use page 0
+	int 0x10
+
+	popa
+	ret
+
+; End of the files
 	times 510 - ($ - $$) db 0		; Pad remainder of boot sector with 0s
 	dw 0xAA55				; The standard PC boot signature
