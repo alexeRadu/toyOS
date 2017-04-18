ffc407cb32ab5916cbf62a3ad18dec8f5ecabd71
Radu Alexe <alexeRadu2007@gmail.com>
2017-04-18 21:52:58 +0300

    kernel: add a linker file
    Signed-off-by: Radu Alexe <alexeRadu2007@gmail.com>

--- kernel/start.asm
+++ kernel/start.asm
@@ -1,5 +1,18 @@
 bits 32
 
-jmp $
+global start
+start:
+	; Set the stack for the kernel.
+	mov esp, sys_stack
+
+	; Infinite loop.
+	jmp $
 
 	times 512 - ($ - $$) db 0
+
+; BSS section. This is where the uninitialized data goes.
+SECTION .bss
+	resb 8192
+
+; This is the stack for the kernel.
+sys_stack:
